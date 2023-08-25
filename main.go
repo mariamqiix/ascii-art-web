b25cb15de2f1078cddc16de8a5dcfbce2f5275f9
rhelal <ruqayahhelal@gmail.com.com>
2023-08-25 07:23:36 +0300

    Final

--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 	"html/template"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 )
 
 type PageData struct {
-	Color string
-	Text  []string
+	Color, ColorBack string
+	Text             []string
 }
 
 func main() {
@@ -20,7 +19,32 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+func CheckBackgroundColor(userColor string) string {
+	Colors := []string{"whitesmoke", "#f5f5f5", "seashell", "#fff5ee", "papayawhip", "#ffefd5",
+		"oldlace", "#fdf5e6", "linen", "#faf0e6", "lightgoldenrodyellow", "#fafad2", "lemonchiffon", "#fffacd",
+		"lavenderblush", "#fff0f5", "cornsilk", "#fff8dc", "blanchedalmond", "#ffebcd", "beige", "#f5f5dc", "antiquewhite",
+		"#faebd7", "#f1f0e8"}
+	for _, color := range Colors {
+		if color == userColor {
+			return "#f1aeb2"
+		}
+	}
+	return "#f1f0e8"
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	text := r.FormValue("thetext")
+	fileName := r.FormValue("chose")
+	_, error := os.Stat(fileName + ".txt")
+	textInASCII := serveIndex(text, fileName)
+	indexTemplate, _ := template.ParseFiles("template/index.html")
+	userColor := strings.ToLower(r.FormValue("color"))
+	backgroundColor := CheckBackgroundColor(userColor)
+	pageData := PageData{
+		Text:      textInASCII,
+		Color:     userColor,
+		ColorBack: backgroundColor,
+	}
 	if r.URL.Path == "/style.css" {
 		http.ServeFile(w, r, "./template/style.css")
 		return
@@ -29,56 +53,18 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./template/404.html")
 		return
 	} else if r.Method == "GET" {
-		indexTemplate, _ := template.ParseFiles("template/index.html")
-		var v []string
-		pageData := PageData{
-			Text:  v,
-			Color: "black",
-		}
 		indexTemplate.Execute(w, pageData)
 		return
-	} else if r.Method != "POST" {
+	} else if r.Method != "POST" || text == "" || !CheckLetter(text) || !CheckColor(userColor) {
 		w.WriteHeader(http.StatusBadRequest)
 		http.ServeFile(w, r, "./template/400.html") // should be 400
 		return
-	} else if len(r.FormValue("thetext")) > 100 {
+	} else if len(r.FormValue("thetext")) > 2000 || os.IsNotExist(error) {
 		w.WriteHeader(http.StatusInternalServerError)
-		http.ServeFile(w, r, "template/400.html")
+		http.ServeFile(w, r, "template/500.html")
 		return
 	}
-	_, error := os.Stat(r.FormValue("chose") + ".txt")
-	if os.IsNotExist(error) {
-		w.WriteHeader(http.StatusNotFound)
-		http.ServeFile(w, r, "./template/500.html")
-		return
-	}
-	if r.FormValue("thetext") == "" {
-		w.WriteHeader(http.StatusInternalServerError)
-		http.ServeFile(w, r, "template/400.html")
-		return
-	}
-	if !CheckLetter(r.FormValue("thetext")) {
-		w.WriteHeader(http.StatusInternalServerError)
-		http.ServeFile(w, r, "template/400.html")
-		return
-	}
-	if !CheckColor(r.FormValue("color")) {
-		w.WriteHeader(http.StatusBadRequest)
-		http.ServeFile(w, r, "./template/400.html")
-		return
-	}
-
-	TextInASCII := serveIndex(r.FormValue("thetext"), r.FormValue("chose"))
-	indexTemplate, _ := template.ParseFiles("template/index.html")
 	if r.Method == "POST" {
-		color := "black"
-		if r.FormValue("color") != "#f1f0e8" {
-			color = r.FormValue("color")
-		}
-		pageData := PageData{
-			Text:  TextInASCII,
-			Color: color,
-		}
 		err := indexTemplate.Execute(w, pageData)
 		if err != nil {
 			fmt.Print(err)
@@ -90,6 +76,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func serveIndex(text, filename string) []string {
 	WordsInArr := strings.Split(text, "\r\n")
 	var Text []string
+	var Words []string
 	for l := 0; l < len(WordsInArr); l++ {
 		var Words [][]string
 		Text1 := strings.ReplaceAll(WordsInArr[l], "\\t", "   ")
@@ -108,23 +95,19 @@ func serveIndex(text, filename string) []string {
 			Text = append(Text, "\n")
 		}
 	}
-	Line := strings.Join(Text, "\n")
-	var Words []string
-	Words = append(Words, Line)
-	return Words
+	return append(Words, strings.Join(Text, "\n"))
 }
 
 func ReadLetter(Text1 byte, fileName string) []string {
-	//buffio object, to open and read the file
 	ReadFile, _ := os.Open(fileName + ".txt")
 	FileScanner := bufio.NewScanner(ReadFile)
 	var Letter []string
 	stop := 1
 	i := 0
-	a := (int(Text1)-32)*9 + 2
+	letterLength := (int(Text1)-32)*9 + 2
 	for FileScanner.Scan() {
 		i++
-		if i >= a {
+		if i >= letterLength {
 			stop++
 			Letter = append(Letter, FileScanner.Text())
 			if stop > 8 {
@@ -149,20 +132,25 @@ func CheckLetter(s string) bool {
 }
 
 func CheckColor(userValue string) bool {
-	Colors := []string{"red", "green", "yellow", "blue", "purple", "cyan", "white", "black", "orange"}
+	Colors := []string{"aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", " blue", "blueviolet", "brown",
+		"burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan",
+		"darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen",
+		"darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray",
+		"darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick", "floralwhite",
+		"forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow",
+		"honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen",
+		"lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen",
+		"lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightsteelblue", "lightyellow",
+		"lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple",
+		"mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue",
+		"mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered",
+		"orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum",
+		"powderblue", "purple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell",
+		"sienna", "silver", "skyblue", "slateblue", "slategray", "snow", "springgreen", "steelblue", "tan", "teal", "thistle",
+		"tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"}
 	for _, color := range Colors {
 		if color == userValue {
 			return true
-		} else if strings.Index(userValue, "rgb") == 0 && userValue[len(userValue)-1] == ')' {
-			userValue := strings.ReplaceAll(userValue, " ", "")
-			c := (strings.Split(userValue[4:len(userValue)-1], ","))
-			for i := 0; i < len(c); i++ {
-				check, err := strconv.Atoi(c[i])
-				if err != nil || len(c) != 3 || check < 0 || check > 255 {
-					return false
-				}
-			}
-			return false
 		} else if strings.Index(userValue, "#") == 0 && len(userValue) == 7 {
 			for i := 1; i <= 6; i++ {
 				if (userValue[i] >= '0' && userValue[i] <= '9') || (userValue[i] >= 'a' && userValue[i] <= 'f') {
